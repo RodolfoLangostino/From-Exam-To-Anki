0bf0172da578ec64cbc905cb8eef611442be6201
Paste <98383107+RodolfoLangostino@users.noreply.github.com>
2024-07-18 21:15:33 +0000

    3  add

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print ("Hello")
+from anki import create_anki_cards
+from pdftotext import pdftotxt
+
+
+input_file = input("Ingrese el nombre del archivo PDF: ")
+output_file = 'anki_cards.csv'
+#print (pdftotxt(input_file))
+skip_pages = input("Ingrese el número de páginas a omitir: ")
+try:  
+  skip_pages = int(skip_pages)
+except ValueError:
+  print("Ingrese un número válido")
+
+input_content = pdftotxt(input_file, skip_pages=3)
+create_anki_cards(input_content, output_file)
